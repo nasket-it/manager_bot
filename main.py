caf682862037d4d51bf98ed0b305bd2a7317f243
Алексей <aleksej@Aleksejs-MacBook-Air.local>
2023-06-21 13:16:56 +0300

    Добавленна отдельная функция статуса приложения

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ def run_sudo_command(command):
     process.wait()
     exit_code = process.returncode
     return exit_code
+
+
+def status_sudo_command(command):
+    full_command = f'echo {Token.sudo_password} | sudo -S {command}'
+    process = subprocess.Popen(full_command, shell=True)
+    process.wait()
+    exit_code = process
+    return exit_code
 # exit_code = run_sudo_command('systemctl stop manager_bot')
 
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.reply("Привет! Отправь /run для запуска скрипта или /stop для его остановки или /status для проверки статуса приложения .")
+
 
 @dp.message_handler(commands=['run'])
 async def run_script(message: types.Message):
@@ -28,17 +37,20 @@ async def run_script(message: types.Message):
     rezult = run_sudo_command(start)
     await message.reply(f"{rezult}")
 
+
 @dp.message_handler(commands=['stop'])
 async def stop_script(message: types.Message):
     stop = 'systemctl stop sanchos'
     # Остановка скрипта
     rezult = run_sudo_command(stop)
     await message.reply(f"{rezult}")
+
+
 @dp.message_handler(commands=['status'])
 async def status_script(message: types.Message):
     status = 'systemctl status sanchos'
     # Остановка скрипта
-    rezult = run_sudo_command(status)
+    rezult =status_sudo_command(status)
     await message.reply(f"{rezult}")
 
 
