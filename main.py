8ee11a291dc825d3f7538a187bf810a75d000f7c
Алексей <aleksej@Aleksejs-MacBook-Air.local>
2023-06-21 12:20:52 +0300

    Добавлена новая функция запуска команд

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import subprocess
-
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from secrete import Token
@@ -9,6 +8,16 @@ bot = Bot(token=Token.bot_token)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+
+def run_sudo_command(command):
+    full_command = f'echo {Token.sudo_password} | sudo -S {command}'
+    process = subprocess.Popen(full_command, shell=True)
+    process.wait()
+    exit_code = process.returncode
+    return exit_code
+exit_code = run_sudo_command('systemctl stop manager_bot')
+print(f"Код завершения: {exit_code}")
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.reply("Привет! Отправь /run для запуска скрипта или /stop для его остановки.")
@@ -16,7 +25,7 @@ async def start(message: types.Message):
 @dp.message_handler(commands=['run'])
 async def run_script(message: types.Message):
     # Запуск скрипта
-    subprocess.Popen(["python", "~/home/sanchos_bot/sanchos/main.py"])
+    subprocess.Popen(["python3", "~/home/sanchos_bot/sanchos/main.py"])
     await message.reply("Скрипт запущен.")
 
 @dp.message_handler(commands=['stop'])
@@ -24,6 +33,17 @@ async def stop_script(message: types.Message):
     # Остановка скрипта
     subprocess.call(["pkill", "-f", "~/home/sanchos_bot/sanchos/main.py"])
     await message.reply("Скрипт остановлен.")
+
+def run_sudo_command(command, password):
+    full_command = f'echo {password} | sudo -S {command}'
+    process = subprocess.Popen(full_command, shell=True)
+    process.wait()
+    exit_code = process.returncode
+    return exit_code
+
+password = "Ваш_пароль"
+exit_code = run_sudo_command('systemctl stop manager_bot', password)
+print(f"Код завершения: {exit_code}")
 
 if __name__ == '__main__':
     # Запуск клиента Telethon
